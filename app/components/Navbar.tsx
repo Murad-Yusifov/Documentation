68d9608b5cb3794873ae69091d256b2a5f4f8851
Murad-Yusifov <muradmy-azmp203@code.edu.az>
2025-08-06 23:30:30 +0400

    add all

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { GrLanguage } from "react-icons/gr";
 import { useAppContext } from "../context/Context";
 import styles from "./Navbar.module.scss";
 const Navbar = () => {
@@ -9,18 +10,44 @@ const Navbar = () => {
 
 
   return (
-    <header className={`w-full flex justify-center `}>
-      <div className={`${styles.content} w-3/4 flex justify-between bg-black text-white rounded-[18px] border-2`} style={{padding:"15px", margin:"10px"}}>
+    <header className={`w-full flex justify-center  ${theme===true? " *:text-black": "*:text-white"} `}>
+      <div className={`${styles.content} w-3/4 flex justify-between items-center bg-black text-white rounded-[18px] border-2 ${theme===true? "bg-white text-black": ""}`} style={{padding:"15px", margin:"10px"}}>
         <div className="">
         <h1>Murad Yusifov</h1>
       </div>
       <nav>
-        <ul className="flex justify-around gap-2">
+        <ul className="w-1/3 flex justify-around gap-2">
           {navBar.map((item, index) => (
             <li key={index}>{item}</li>
           ))}
         </ul>
       </nav>
+
+      <div className="flex  w-1/4 justify-around items-center">
+        <div className="flex items-center justify-center gap-2">
+          <select
+          value={language}
+          onChange={(e)=> setLanguage(e.target.value)}
+          className={`${theme===true? "*:bg-white *:text-black":"*:bg-black *:text-white"}`}>
+            
+            <option value="en">En</option>
+            <option value="az">Az</option>
+            <option value="rus">Rus</option>
+            <option value="tr">Tr</option>
+          </select>
+
+          <GrLanguage />
+        </div>
+       <div className={styles.btnContainer}>
+         <button
+        onClick={()=>setTheme(!theme)}
+        className={`bg-transparent border-2 border-white text-lack px-4 py-2 rounded-lg ${theme===false? "bg-white text-black border-black": ""}  ${styles.btn}`}
+        >
+          {theme ===true? "Light Mode" : "Dark Mode"}
+
+        </button>
+       </div>
+      </div>
       </div>
     </header>
   );
--- app/context/Context.tsx
+++ app/context/Context.tsx
@@ -1,13 +1,13 @@
 'use client';
 
-import { createContext, useContext, useState, ReactNode } from 'react';
+import { createContext, useContext, useState, ReactNode, useEffect } from 'react';
 
 // Tip tərifi
 type AppContextType = {
   theme: boolean;
   setTheme: (value: boolean) => void;
-  language: string | null;
-  setLanguage: (value: string | null) => void;
+  language: string;
+  setLanguage: (value: string) => void;
 };
 
 // Context yaradılır
@@ -16,7 +16,26 @@ const AppContext = createContext<AppContextType | undefined>(undefined);
 // Provider
 export const AppProvider = ({ children }: { children: ReactNode }) => {
   const [theme, setTheme] = useState<boolean>(false);
-  const [language, setLanguage] = useState<string | null>(null); // düz yazılış!
+  const [language, setLanguage] = useState<string>(""); // düz yazılış!
+
+useEffect(() => {
+  const savedTheme = sessionStorage.getItem('theme');
+  const savedLanguage = sessionStorage.getItem('language');
+
+  if (savedTheme) {
+    setTheme(savedTheme === "true");
+  }
+  if (savedLanguage) {
+    setLanguage(savedLanguage);
+  }
+}, []);
+
+
+ useEffect(() => {
+  sessionStorage.setItem('theme', theme.toString());
+  sessionStorage.setItem('language', language);
+}, [theme, language]);
+
 
   return (
     <AppContext.Provider value={{ theme, setTheme, language, setLanguage }}>
